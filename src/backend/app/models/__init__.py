2087ee11b7fb9f45d3447e48445583f044e5bd4c
Federico Melo <f.melo@uniandes.edu.co>
2026-03-03 22:05:06 -0500

    Add init for models directory

---
+++ src/backend/app/models/__init__.py
@@ -0,0 +1,109 @@
+from app.models.activity import (
+    ActivitiesPublic,
+    Activity,
+    ActivityCreate,
+    ActivityPublic,
+    ActivityUpdate,
+)
+from app.models.developer_data_id import (
+    DeveloperDataId,
+    DeveloperDataIdCreate,
+    DeveloperDataIdPublic,
+    DeveloperDataIdsPublic,
+    DeveloperDataIdUpdate,
+)
+from app.models.device_info import (
+    DeviceInfo,
+    DeviceInfoCreate,
+    DeviceInfoPublic,
+    DeviceInfosPublic,
+    DeviceInfoUpdate,
+)
+from app.models.event import (
+    Event,
+    EventCreate,
+    EventPublic,
+    EventsPublic,
+    EventUpdate,
+)
+from app.models.file_id import (
+    FileId,
+    FileIdCreate,
+    FileIdPublic,
+    FileIdUpdate,
+)
+from app.models.fit_file import (
+    FitFile,
+    FitFileCreate,
+    FitFilePublic,
+    FitFilesPublic,
+    FitFileUpdate,
+)
+from app.models.lap import (
+    Lap,
+    LapCreate,
+    LapPublic,
+    LapsPublic,
+    LapUpdate,
+)
+from app.models.record import (
+    Record,
+    RecordCreate,
+    RecordPublic,
+    RecordsPublic,
+    RecordUpdate,
+)
+from app.models.session import (
+    Session,
+    SessionCreate,
+    SessionPublic,
+    SessionsPublic,
+    SessionUpdate,
+)
+
+__all__ = [
+    "ActivitiesPublic",
+    "Activity",
+    "ActivityCreate",
+    "ActivityPublic",
+    "ActivityUpdate",
+    "DeveloperDataId",
+    "DeveloperDataIdCreate",
+    "DeveloperDataIdPublic",
+    "DeveloperDataIdsPublic",
+    "DeveloperDataIdUpdate",
+    "DeviceInfo",
+    "DeviceInfoCreate",
+    "DeviceInfoPublic",
+    "DeviceInfosPublic",
+    "DeviceInfoUpdate",
+    "Event",
+    "EventCreate",
+    "EventPublic",
+    "EventsPublic",
+    "EventUpdate",
+    "FileId",
+    "FileIdCreate",
+    "FileIdPublic",
+    "FileIdUpdate",
+    "FitFile",
+    "FitFileCreate",
+    "FitFilePublic",
+    "FitFilesPublic",
+    "FitFileUpdate",
+    "Lap",
+    "LapCreate",
+    "LapPublic",
+    "LapsPublic",
+    "LapUpdate",
+    "Record",
+    "RecordCreate",
+    "RecordPublic",
+    "RecordsPublic",
+    "RecordUpdate",
+    "Session",
+    "SessionCreate",
+    "SessionPublic",
+    "SessionsPublic",
+    "SessionUpdate",
+]
